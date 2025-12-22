bd09cb5682b9091aedaa65b686e9cf18d5600e58
Arthur97172 <arthur97172@outlook.com>
2025-12-22 13:38:41 +0800

    Update 99-custom.sh

--- files/uci-defaults/99-custom.sh
+++ files/uci-defaults/99-custom.sh
@@ -5,11 +5,6 @@ echo "Starting 99-custom.sh at $(date)" >>$LOGFILE
 # 设置默认防火墙规则，方便虚拟机首次访问 WebUI
 uci set firewall.@zone[1].input='ACCEPT'
 
-# 设置主机名映射，解决安卓原生 TV 无法联网的问题
-#uci add dhcp domain
-#uci set "dhcp.@domain[-1].name=time.android.com"
-#uci set "dhcp.@domain[-1].ip=203.107.6.88"
-
 # 设置主机名
 uci set system.@system[0].hostname='iStoreOS'
 uci set system.@system[0].timezone='CST-8'
@@ -23,12 +18,6 @@ uci commit luci
 
 # 设置所有网口可访问网页终端
 uci delete ttyd.@ttyd[0].interface
-
-# 设置编译作者信息
-#FILE_PATH="/etc/openwrt_release"
-#NEW_DESCRIPTION="Packaged by Arthur"
-#sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
-
 
 # 计算网卡数量
 count=0
